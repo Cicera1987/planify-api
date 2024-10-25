66e228d3c80c1444cc2ae747317b250ebfa09a87
Cicera Ribeiro <maccicera@gmail.com>
2024-10-25 11:01:40 -0300

    refactor: refector in functions services packages

--- src/app/modules/servicePackages/entities/service-package.entity.ts
+++ src/app/modules/servicePackages/entities/service-package.entity.ts
@@ -1,17 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { IProcedure } from 'app/modules/procedures/interfaces/procedure.interface';
-import mongoose, { Document, Types } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
 export type ServicePackageDocument = ServicePackage & Document;
-
 @Schema()
 export class ServicePackage {
   @Prop({
     type: String,
-    default: uuidv4,
+    default: () => uuidv4(),
   })
-  id: string;
+  _id: string;
+
   @Prop({ required: true })
   value: number;
 
@@ -22,7 +22,7 @@ export class ServicePackage {
   procedures: IProcedure[];
 
   constructor(name: string, procedures: IProcedure[], value: number) {
-    this.id = uuidv4();
+    this._id = uuidv4();
     this.name = name;
     this.procedures = procedures;
     this.value = value;
--- src/app/modules/servicePackages/services/service-package.service.ts
+++ src/app/modules/servicePackages/services/service-package.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
+import { Model } from 'mongoose';
 import {
   ServicePackage,
   ServicePackageDocument,
 } from '../entities/service-package.entity';
-import { Procedure } from 'app/modules/procedures/entities/procedure.entity';
 import { CreateServicePackageDto } from '../dtos/create.service-package';
 
 @Injectable()
@@ -13,7 +16,6 @@ export class ServicePackageService {
   constructor(
     @InjectModel(ServicePackage.name)
     private packageModel: Model<ServicePackageDocument>,
-    @InjectModel(Procedure.name) private procedureModel: Model<Procedure>,
   ) {}
 
   async create(
@@ -21,7 +23,6 @@ export class ServicePackageService {
   ): Promise<ServicePackage> {
     const { name, value, procedures } = createServicePackageDto;
 
-    // Verifique se procedures é realmente um array antes de iterar
     if (!Array.isArray(procedures)) {
       throw new TypeError('Procedures must be an array of IDs.');
     }
@@ -36,24 +37,41 @@ export class ServicePackageService {
   }
 
   async findAll(): Promise<ServicePackage[]> {
-    return this.packageModel.find().populate('procedures', 'id name').exec(); // Popula os dados de procedures com apenas `id` e `name`
+    try {
+      const servicePackages = await this.packageModel
+        .find()
+        .populate('procedures', 'id name')
+        .exec();
+
+      return servicePackages;
+    } catch (error) {
+      console.error('Error retrieving service packages:', error);
+      throw new InternalServerErrorException(
+        'An error occurred while retrieving service packages.',
+      );
+    }
   }
 
   async findOne(id: string): Promise<ServicePackage> {
-    const servicePackage = await this.packageModel
-      .findById(id)
-      .populate('procedures', 'id name')
-      .exec();
-    if (!servicePackage) {
-      throw new NotFoundException(`Package with ID ${id} not found.`);
-    }
-    return servicePackage;
-  }
+    try {
+      const servicePackage = await this.packageModel
+        .findOne({ _id: id })
+        .populate('procedures', 'id name')
+        .exec();
 
-  async remove(id: string): Promise<void> {
-    const result = await this.packageModel.findByIdAndDelete(id).exec();
-    if (!result) {
-      throw new NotFoundException(`Package with ID ${id} not found.`);
+      if (!servicePackage) {
+        throw new NotFoundException(`Package with ID ${id} not found.`);
+      }
+
+      return servicePackage;
+    } catch (error) {
+      console.error('Error finding package:', error);
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'An error occurred while finding the package.',
+      );
     }
   }
 
@@ -61,12 +79,36 @@ export class ServicePackageService {
     id: string,
     updatePackageDto: CreateServicePackageDto,
   ): Promise<ServicePackage> {
-    const updatedPackage = await this.packageModel
-      .findByIdAndUpdate(id, updatePackageDto, { new: true })
-      .exec();
-    if (!updatedPackage) {
-      throw new NotFoundException(`Package with ID ${id} not found.`);
+    try {
+      const updatedPackage = await this.packageModel
+        .findOneAndUpdate({ _id: id }, updatePackageDto, { new: true })
+        .exec();
+
+      if (!updatedPackage) {
+        throw new NotFoundException(`Package with ID ${id} not found.`);
+      }
+
+      return updatedPackage;
+    } catch (error) {
+      console.error('Error updating package:', error);
+      throw new InternalServerErrorException(
+        'An error occurred while updating the package.',
+      );
     }
-    return updatedPackage;
+  }
+
+  async remove(id: string): Promise<void> {
+    try {
+      const result = await this.packageModel.findOneAndDelete({ id }).exec();
+
+      if (!result) {
+        throw new NotFoundException(`Package with ID ${id} not found.`);
+      }
+    } catch (error) {
+      console.error('Error removing package:', error);
+      throw new InternalServerErrorException(
+        'An error occurred while removing the package.',
+      );
+    }
   }
 }
