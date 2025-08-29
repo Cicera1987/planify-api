d14f686c77ec4eb9c78784728d2509f5b79ac23a
Cicera Ribeiro <maccicera@gmail.com>
2025-08-29 15:15:57 -0300

    feat: liberando url do frontend

--- src/main/java/com/tcc/planify_api/security/SecurityConfiguration.java
+++ src/main/java/com/tcc/planify_api/security/SecurityConfiguration.java
@@ -66,8 +66,11 @@ public class SecurityConfiguration {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+              .allowedOrigins("http://localhost:3000")
               .allowedMethods("*")
-              .exposedHeaders("Authorization");
+              .allowedHeaders("*")
+              .exposedHeaders("Authorization")
+              .allowCredentials(true);
       }
     };
   }
