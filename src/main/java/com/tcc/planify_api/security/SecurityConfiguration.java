ebae34747bedca262e857db601a6c6431b24df99
Cicera Ribeiro <maccicera@gmail.com>
2025-08-29 15:54:20 -0300

    fix, ajustes do methods

--- src/main/java/com/tcc/planify_api/security/SecurityConfiguration.java
+++ src/main/java/com/tcc/planify_api/security/SecurityConfiguration.java
@@ -67,7 +67,6 @@ public class SecurityConfiguration {
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
               .allowedOrigins("http://localhost:3000")
-              .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
               .allowedMethods("*")
               .allowedHeaders("*")
               .exposedHeaders("Authorization")
