dc2fae8c57b2b76d3cf4e58436cec3413cb05fbd
Cicera Ribeiro <maccicera@gmail.com>
2025-09-12 09:53:28 -0300

    fix: ajuste userDTO

--- src/main/java/com/tcc/planify_api/dto/user/UserDTO.java
+++ src/main/java/com/tcc/planify_api/dto/user/UserDTO.java
@@ -13,7 +13,6 @@ public class UserDTO {
   private String username;
   private String email;
   private String phone;
-  private String password;
   private String speciality;
   private PositionEnum position;
   private String imageUrl;
