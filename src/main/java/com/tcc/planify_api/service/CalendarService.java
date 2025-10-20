b3dcfbf6a0356f75b8c0a8ceb3547dc6b4d1d619
Cicera Ribeiro <maccicera@gmail.com>
2025-10-20 16:53:33 -0300

    feat: ajuste order times

--- src/main/java/com/tcc/planify_api/service/CalendarService.java
+++ src/main/java/com/tcc/planify_api/service/CalendarService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -131,9 +132,13 @@ public class CalendarService {
     dto.setId(day.getId());
     dto.setUserId(day.getUserId());
     dto.setLocalDate(day.getLocalDate());
-    dto.setTimes(day.getTimes().stream()
+
+    List<CalendarTimeDTO> sortedTimes = day.getTimes().stream()
+          .sorted(Comparator.comparing(CalendarTimeEntity::getTime))
           .map(this::mapTimeToDTO)
-          .collect(Collectors.toList()));
+          .collect(Collectors.toList());
+
+    dto.setTimes(sortedTimes);
     return dto;
   }
 
@@ -143,4 +148,5 @@ public class CalendarService {
     dto.setTime(time.getTime());
     return dto;
   }
+
 }
